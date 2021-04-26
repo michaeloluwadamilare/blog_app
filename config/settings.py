eeec7b7c2a254c24d9fe3f54df7117a1a100ad0f
michaeloluwadamilare <63959057+michaeloluwadamilare@users.noreply.github.com>
2021-04-26 02:31:17 +0100

    'new'

--- config/settings.py
+++ config/settings.py
@@ -20,10 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l##-+1b@bm^4@j&k^-(pz7$x!k(0x0u6mb_6zbltwn@-uv83uj'
+#SECRET_KEY = 'django-insecure-l##-+1b@bm^4@j&k^-(pz7$x!k(0x0u6mb_6zbltwn@-uv83uj'
+
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-l##-+1b@bm^4@j&k^-(pz7$x!k(0x0u6mb_6zbltwn@-uv83uj')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
